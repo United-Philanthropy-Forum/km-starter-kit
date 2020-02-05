64f12223a35d096130ab45d870145c7879daf354
Maria Fisher <maria.fisher@thinkshout.org>
2020-02-04 18:43:15 -0800

    Making the copied files vary based on which composer command you ran.

--- scripts/composer/ScriptUpdater.php
+++ scripts/composer/ScriptUpdater.php
@@ -27,16 +27,17 @@ class ScriptUpdater
     $drops_file = '/vendor/pantheon-systems/example-drops-8-composer';
     $ignored_files = $event->getComposer()->getPackage()->getExtra() + ['km-collab-scaffold' => [ 'excludes' => []]];
     $pantheon_install = $root . $drops_file;
+    $event_name = $event->getName();
 
-    if ($fs->exists($pantheon_install)) {
+    if ($fs->exists($pantheon_install) && isset($ignored_files['km-collab-scaffold']['excludes'][$event_name])) {
       $iterator = $finder->files()->in($pantheon_install);
 
-      foreach ($ignored_files['km-collab-scaffold']['excludes'] as $ignore_file) {
+      foreach ($ignored_files['km-collab-scaffold']['excludes'][$event_name] as $ignore_file) {
         $iterator->notName($pantheon_install . '/' . $ignore_file);
       }
 
       foreach ($iterator as $file) {
-        if (!in_array($file->getBasename(), $ignored_files['km-collab-scaffold']['excludes'])) {
+        if (!in_array($file->getBasename(), $ignored_files['km-collab-scaffold']['excludes'][$event_name])) {
           $vendor_path = $file->getPathname();
           $destination = str_replace($drops_file, '', $vendor_path);
           $fs->copy($vendor_path, $destination, TRUE);
